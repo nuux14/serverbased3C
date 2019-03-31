09d488d7a47ec6a0b4a5309b5c435bd3c9705fc4
nuux14 <nuux14@gmail.com>
2019-03-31 16:44:42 +0300

    jsFundamentals

---
+++ js/jsFund.js
@@ -0,0 +1,8 @@
+
+function calculate(){
+var n1= document.getElementById("txtN1"),
+ n2 = document.getElementById("txtN2"),
+ res = document.getElementById("result");
+ res.innerText = Number(n1.value)+Number(n2.value);
+
+}
