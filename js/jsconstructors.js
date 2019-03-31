c76a695184f976b8ba1d781dcc68b3bd9b3674de
nuux14 <nuux14@gmail.com>
2019-03-31 18:06:23 +0300

    constructors

---
+++ js/jsconstructors.js
@@ -0,0 +1,36 @@
+function addUser(){
+    var name = document.getElementById("txtName");
+    var email = document.querySelector('#txtEmail');
+    var tbl = document.querySelector("#tbl");
+  //create new row
+    var tr= document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+
+    td1.innerText = name.value;
+    td2.innerText = email.value;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    tbl.appendChild(tr);
+   var nuser = new User(name.value,email.value);
+   usersCollection.push(nuser);
+    name.value='';
+    email.value='';
+}
+// var person={fname:"Gedi",lname:"Ismail"};
+// var person2={};
+//Constructors;
+var usersCollection=[];
+
+function User(name,email){
+    this.fname=name;
+    this.userEmail=email;
+}
+
+// var u = new User("Hinda", "hinda@g.com");
+// var u2 = new User("Gedi","gedi@g.com");
+
+//JSON- Javascript Object Notation
+//      exchange data bn sites.
